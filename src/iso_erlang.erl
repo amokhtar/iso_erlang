c639ba6364fcc23e956168c82a544a5f65a9bf8a
Ahmed Mokhtar <ahmed.mokhtar@vungle.com>
2016-06-29 18:36:48 -0700

    Adds conversion, tests and validation

--- src/iso_erlang.erl
+++ src/iso_erlang.erl
@@ -23,12 +23,17 @@
     to_country_name/1,
     country_to_numerical_code/1,
 
+    language_alpha_2_to_name/1,
+    language_alpha_3_to_name/1,
     get_alpha_2_language_list/0,
     get_alpha_3_language_list/0,
     get_language_name_list/0,
     is_language/1,
     is_language_alpha_2/1,
-    is_language_alpha_3/1
+    is_language_alpha_3/1,
+    to_language_alpha_2/1,
+    to_language_alpha_3/1,
+    to_language_name/1
 ]).
 
 %% @doc Returns a list of all countries in ISO alpha-2 format
@@ -99,7 +104,7 @@ to_country_alpha_2(Country) when is_list(Country) orelse is_bitstring(Country) -
     NormalizedCountry = normalize(Country),
     case is_country_alpha_2(NormalizedCountry) of
         true -> NormalizedCountry;
-        false -> iso_erlang_country_converter:to_country_alpha_2_upper(NormalizedCountry)
+        false -> iso_erlang_country_converter:to_alpha_2_upper(NormalizedCountry)
     end.
 
 %% @doc Converts two letter country code (ISO alpha-2) to the equivalent country name
@@ -110,19 +115,19 @@ country_alpha_2_to_name(Country) when is_list(Country) orelse is_bitstring(Count
 %% @doc Converts three letter country code (ISO alpha-3) to the equivalent country name
 %% @end
 -spec country_alpha_3_to_name(bitstring() | string()) -> bitstring().
-country_alpha_3_to_name(Country) when is_list(Country) orelse is_bitstring(Country) -> iso_erlang_country_converter:country_alpha_3_to_name_upper(normalize(Country)).
+country_alpha_3_to_name(Country) when is_list(Country) orelse is_bitstring(Country) -> iso_erlang_country_converter:alpha_3_to_name_upper(normalize(Country)).
 
 %% @doc Converts three letter country code (ISO alpha-3) to the equivalent United Nations numerical code M49 for countries.
 %% @end
 -spec country_alpha_3_to_numerical_code(bitstring() | string()) -> integer().
 country_alpha_3_to_numerical_code(Country) when is_list(Country) orelse is_bitstring(Country) ->
-    iso_erlang_country_converter:country_alpha_3_to_numerical_code_upper(normalize(Country)).
+    iso_erlang_country_converter:alpha_3_to_numerical_code_upper(normalize(Country)).
 
 %% @doc Converts two letter country code (ISO alpha-2) to the equivalent United Nations numerical code M49 for countries.
 %% @end
 -spec country_alpha_2_to_numerical_code(bitstring() | string()) -> integer().
 country_alpha_2_to_numerical_code(Country) when is_list(Country) orelse is_bitstring(Country) ->
-    iso_erlang_country_converter:country_alpha_2_to_numerical_code_upper(normalize(Country)).
+    iso_erlang_country_converter:alpha_2_to_numerical_code_upper(normalize(Country)).
 
 %% @doc Returns a list of all countries in ISO alpha-2 format
 %% @end
@@ -157,6 +162,42 @@ is_language_alpha_3(Language) when is_list(Language) orelse is_bitstring(Languag
 %% @end
 -spec is_language_alpha_2(bitstring() | string()) -> boolean().
 is_language_alpha_2(Language) when is_list(Language) orelse is_bitstring(Language) -> iso_erlang_language_validator:is_language_alpha_2_upper(normalize(Language)).
+
+%% @doc Converts two letter Language code (ISO alpha-2) to the equivalent three letter Language code (ISO alpha-3)
+%% @end
+-spec to_language_alpha_3(bitstring() | string()) -> bitstring().
+to_language_alpha_3(Language) when is_list(Language) orelse is_bitstring(Language) ->
+    NormalizedLanguage = normalize(Language),
+    case is_language_alpha_3(NormalizedLanguage) of
+        true -> NormalizedLanguage;
+        false -> iso_erlang_language_converter:to_alpha_3_upper(NormalizedLanguage)
+    end.
+
+%% @doc Converts three letter Language code (ISO alpha-3) to the equivalent two letter Language code (ISO alpha-2)
+%% @end
+-spec to_language_alpha_2(bitstring() | string()) -> bitstring().
+to_language_alpha_2(Language) when is_list(Language) orelse is_bitstring(Language) ->
+    NormalizedLanguage = normalize(Language),
+    case is_language_alpha_2(NormalizedLanguage) of
+        true -> NormalizedLanguage;
+        false -> iso_erlang_language_converter:to_alpha_2_upper(NormalizedLanguage)
+    end.
+
+%% @doc Converts two letter language codes (ISO alpha-2) and the three letter language codes (ISO alpha-3)
+%%      to language names.
+%% @end
+-spec to_language_name(bitstring() | string()) -> bitstring().
+to_language_name(Language) when is_list(Language) orelse is_bitstring(Language) -> iso_erlang_language_converter:to_language_name_upper(normalize(Language)).
+
+%% @doc Converts two letter language code (ISO alpha-2) to the equivalent language name
+%% @end
+-spec language_alpha_2_to_name(bitstring() | string()) -> bitstring().
+language_alpha_2_to_name(Language) when is_list(Language) orelse is_bitstring(Language) -> iso_erlang_language_converter:alpha_2_to_name_upper(normalize(Language)).
+
+%% @doc Converts three letter language code (ISO alpha-3) to the equivalent language name
+%% @end
+-spec language_alpha_3_to_name(bitstring() | string()) -> bitstring().
+language_alpha_3_to_name(Language) when is_list(Language) orelse is_bitstring(Language) -> iso_erlang_language_converter:alpha_3_to_name_upper(normalize(Language)).
 
 normalize(String) when is_list(String) ->
     list_to_bitstring(string:to_upper(String));
--- test/iso_erlang_language_test.erl
+++ test/iso_erlang_language_test.erl
@@ -44,134 +44,93 @@ is_language_true_test() ->
     ?assertEqual(true, iso_erlang:is_language("En")),
     ?assertEqual(true, iso_erlang:is_language("fR")).
 
-is_country_false_test() ->
+is_language_false_test() ->
     ?assertEqual(false, iso_erlang:is_language(<<"EGGEGG">>)),
     ?assertEqual(false, iso_erlang:is_language(<<"">>)),
     ?assertEqual(false, iso_erlang:is_language("UadfsSs")).
-%%---------------------------------------------------------------
-%%
-%%to_alpha_2_correct_test() ->
-%%    ?assertEqual(<<"EG">>, iso_erlang:to_country_alpha_2(<<"EGY">>)),
-%%    ?assertEqual(<<"US">>, iso_erlang:to_country_alpha_2(<<"USA">>)),
-%%    ?assertEqual(<<"EG">>, iso_erlang:to_country_alpha_2("EGY")),
-%%    ?assertEqual(<<"US">>, iso_erlang:to_country_alpha_2("USA")).
-%%
-%%to_alpha_2_already_correct_test() ->
-%%    ?assertEqual(<<"EG">>, iso_erlang:to_country_alpha_2(<<"EG">>)),
-%%    ?assertEqual(<<"US">>, iso_erlang:to_country_alpha_2(<<"US">>)),
-%%    ?assertEqual(<<"EG">>, iso_erlang:to_country_alpha_2("EG")),
-%%    ?assertEqual(<<"US">>, iso_erlang:to_country_alpha_2("US")).
-%%
-%%to_alpha_2_incorrect_test() ->
-%%    ?assertError(_, iso_erlang:to_country_alpha_2(<<"EGR">>)),
-%%    ?assertError(_, iso_erlang:to_country_alpha_2(<<"AAA">>)),
-%%    ?assertError(_, iso_erlang:to_country_alpha_2("EGs")),
-%%    ?assertError(_, iso_erlang:to_country_alpha_2("AAA")).
-%%
-%%to_alpha_3_correct_test() ->
-%%    ?assertEqual(<<"EGY">>, iso_erlang:to_country_alpha_3(<<"EG">>)),
-%%    ?assertEqual(<<"USA">>, iso_erlang:to_country_alpha_3(<<"US">>)),
-%%    ?assertEqual(<<"EGY">>, iso_erlang:to_country_alpha_3("EG")),
-%%    ?assertEqual(<<"USA">>, iso_erlang:to_country_alpha_3("US")).
-%%
-%%to_alpha_3_already_correct_test() ->
-%%    ?assertEqual(<<"EGY">>, iso_erlang:to_country_alpha_3(<<"EGY">>)),
-%%    ?assertEqual(<<"USA">>, iso_erlang:to_country_alpha_3(<<"USA">>)),
-%%    ?assertEqual(<<"EGY">>, iso_erlang:to_country_alpha_3("EGY")),
-%%    ?assertEqual(<<"USA">>, iso_erlang:to_country_alpha_3("USA")).
-%%
-%%to_alpha_3_incorrect_test() ->
-%%    ?assertError(_, iso_erlang:to_country_alpha_3(<<"EGR">>)),
-%%    ?assertError(_, iso_erlang:to_country_alpha_3(<<"AAA">>)),
-%%    ?assertError(_, iso_erlang:to_country_alpha_3("EGs")),
-%%    ?assertError(_, iso_erlang:to_country_alpha_3("AAA")).
-%%
-%%alpha_2_to_name_correct_test() ->
-%%    ?assertEqual(<<"Egypt">>, iso_erlang:country_alpha_2_to_name(<<"EG">>)),
-%%    ?assertEqual(<<"United States of America">>, iso_erlang:country_alpha_2_to_name(<<"US">>)),
-%%    ?assertEqual(<<"Egypt">>, iso_erlang:country_alpha_2_to_name("EG")),
-%%    ?assertEqual(<<"United States of America">>, iso_erlang:country_alpha_2_to_name("US")).
-%%
-%%alpha_2_to_name_incorrect_test() ->
-%%    ?assertError(_, iso_erlang:country_alpha_2_to_name(<<"EGY">>)),
-%%    ?assertError(_, iso_erlang:country_alpha_2_to_name(<<"USA">>)),
-%%    ?assertError(_, iso_erlang:country_alpha_2_to_name("ABC")),
-%%    ?assertError(_, iso_erlang:country_alpha_2_to_name("AAA")).
-%%
-%%alpha_3_to_name_correct_test() ->
-%%    ?assertEqual(<<"Egypt">>, iso_erlang:country_alpha_3_to_name(<<"EGY">>)),
-%%    ?assertEqual(<<"United States of America">>, iso_erlang:country_alpha_3_to_name(<<"USA">>)),
-%%    ?assertEqual(<<"Egypt">>, iso_erlang:country_alpha_3_to_name("EGY")),
-%%    ?assertEqual(<<"United States of America">>, iso_erlang:country_alpha_3_to_name("USA")).
-%%
-%%alpha_3_to_name_incorrect_test() ->
-%%    ?assertError(_, iso_erlang:country_alpha_3_to_name(<<"EG">>)),
-%%    ?assertError(_, iso_erlang:country_alpha_3_to_name(<<"US">>)),
-%%    ?assertError(_, iso_erlang:country_alpha_3_to_name("ABA")),
-%%    ?assertError(_, iso_erlang:country_alpha_3_to_name("AA")).
-%%
-%%to_country_name_correct_test() ->
-%%    ?assertEqual(<<"Egypt">>, iso_erlang:to_country_name(<<"EGY">>)),
-%%    ?assertEqual(<<"United States of America">>, iso_erlang:to_country_name(<<"USA">>)),
-%%    ?assertEqual(<<"Egypt">>, iso_erlang:to_country_name("EGY")),
-%%    ?assertEqual(<<"United States of America">>, iso_erlang:to_country_name("USA")),
-%%    ?assertEqual(<<"Egypt">>, iso_erlang:to_country_name(<<"EG">>)),
-%%    ?assertEqual(<<"United States of America">>, iso_erlang:to_country_name(<<"US">>)),
-%%    ?assertEqual(<<"Egypt">>, iso_erlang:to_country_name("EG")),
-%%    ?assertEqual(<<"United States of America">>, iso_erlang:to_country_name("US")).
-%%
-%%to_country_name_incorrect_test() ->
-%%    ?assertError(_, iso_erlang:to_country_name(<<"ABC">>)),
-%%    ?assertError(_, iso_erlang:to_country_name(<<"AAA">>)),
-%%    ?assertError(_, iso_erlang:to_country_name("ABC")),
-%%    ?assertError(_, iso_erlang:to_country_name("AAA")).
-%%
-%%alpha_2_to_numerical_code_correct_test() ->
-%%    ?assertEqual(818, iso_erlang:country_alpha_2_to_numerical_code(<<"EG">>)),
-%%    ?assertEqual(840, iso_erlang:country_alpha_2_to_numerical_code(<<"US">>)),
-%%    ?assertEqual(818, iso_erlang:country_alpha_2_to_numerical_code("EG")),
-%%    ?assertEqual(840, iso_erlang:country_alpha_2_to_numerical_code("US")).
-%%
-%%alpha_2_to_numerical_code_incorrect_test() ->
-%%    ?assertError(_, iso_erlang:country_alpha_2_to_numerical_code(<<"EGY">>)),
-%%    ?assertError(_, iso_erlang:country_alpha_2_to_numerical_code(<<"USA">>)),
-%%    ?assertError(_, iso_erlang:country_alpha_2_to_numerical_code("EGY")),
-%%    ?assertError(_, iso_erlang:country_alpha_2_to_numerical_code("USA")).
-%%
-%%alpha_3_to_numerical_code_correct_test() ->
-%%    ?assertEqual(818, iso_erlang:country_alpha_3_to_numerical_code(<<"EGY">>)),
-%%    ?assertEqual(840, iso_erlang:country_alpha_3_to_numerical_code(<<"USA">>)),
-%%    ?assertEqual(818, iso_erlang:country_alpha_3_to_numerical_code("EGY")),
-%%    ?assertEqual(840, iso_erlang:country_alpha_3_to_numerical_code("USA")).
-%%
-%%alpha_3_to_numerical_code_incorrect_test() ->
-%%    ?assertError(_, iso_erlang:country_alpha_3_to_numerical_code(<<"EG">>)),
-%%    ?assertError(_, iso_erlang:country_alpha_3_to_numerical_code(<<"US">>)),
-%%    ?assertError(_, iso_erlang:country_alpha_3_to_numerical_code("EG")),
-%%    ?assertError(_, iso_erlang:country_alpha_3_to_numerical_code("US")).
-%%
-%%to_numerical_code_correct_test() ->
-%%    ?assertEqual(818, iso_erlang:country_to_numerical_code(<<"EGY">>)),
-%%    ?assertEqual(840, iso_erlang:country_to_numerical_code(<<"USA">>)),
-%%    ?assertEqual(818, iso_erlang:country_to_numerical_code("EGY")),
-%%    ?assertEqual(840, iso_erlang:country_to_numerical_code("USA")),
-%%    ?assertEqual(818, iso_erlang:country_to_numerical_code(<<"EG">>)),
-%%    ?assertEqual(840, iso_erlang:country_to_numerical_code(<<"US">>)),
-%%    ?assertEqual(818, iso_erlang:country_to_numerical_code("EG")),
-%%    ?assertEqual(840, iso_erlang:country_to_numerical_code("US")).
-%%
-%%to_numerical_code_incorrect_test() ->
-%%    ?assertError(_, iso_erlang:country_to_numerical_code(<<"EsdG">>)),
-%%    ?assertError(_, iso_erlang:country_to_numerical_code(<<"UsS">>)),
-%%    ?assertError(_, iso_erlang:country_to_numerical_code("EGs")),
-%%    ?assertError(_, iso_erlang:country_to_numerical_code("sUS")).
-%%
-%%normalize_test() ->
-%%    ?assertEqual(<<"HELLO">>, iso_erlang:normalize(<<"Hello">>)),
-%%    ?assertEqual(<<"HELLO">>, iso_erlang:normalize(<<"HeLlO">>)),
-%%    ?assertEqual(<<"HELLO">>, iso_erlang:normalize("hello")),
-%%    ?assertEqual(<<"HELLO">>, iso_erlang:normalize("HeLlO")).
-%%
+
+to_alpha_2_correct_test() ->
+    ?assertEqual(<<"EN">>, iso_erlang:to_language_alpha_2(<<"ENG">>)),
+    ?assertEqual(<<"FR">>, iso_erlang:to_language_alpha_2(<<"FRA">>)),
+    ?assertEqual(<<"EN">>, iso_erlang:to_language_alpha_2("ENG")),
+    ?assertEqual(<<"FR">>, iso_erlang:to_language_alpha_2("FRE")).
+
+to_alpha_2_already_correct_test() ->
+    ?assertEqual(<<"EN">>, iso_erlang:to_language_alpha_2(<<"EN">>)),
+    ?assertEqual(<<"FR">>, iso_erlang:to_language_alpha_2(<<"FR">>)),
+    ?assertEqual(<<"EN">>, iso_erlang:to_language_alpha_2("EN")),
+    ?assertEqual(<<"FR">>, iso_erlang:to_language_alpha_2("FR")).
+
+to_alpha_2_incorrect_test() ->
+    ?assertError(_, iso_erlang:to_language_alpha_2(<<"EGR">>)),
+    ?assertError(_, iso_erlang:to_language_alpha_2(<<"AAA">>)),
+    ?assertError(_, iso_erlang:to_language_alpha_2("EGs")),
+    ?assertError(_, iso_erlang:to_language_alpha_2("AAA")).
+
+to_alpha_3_correct_test() ->
+    ?assertEqual(<<"ENG">>, iso_erlang:to_language_alpha_3(<<"EN">>)),
+    ?assertEqual(<<"FRA">>, iso_erlang:to_language_alpha_3(<<"FR">>)),
+    ?assertEqual(<<"ENG">>, iso_erlang:to_language_alpha_3("EN")),
+    ?assertEqual(<<"FRA">>, iso_erlang:to_language_alpha_3("FR")).
+
+to_alpha_3_already_correct_test() ->
+    ?assertEqual(<<"ENG">>, iso_erlang:to_language_alpha_3(<<"ENG">>)),
+    ?assertEqual(<<"FRA">>, iso_erlang:to_language_alpha_3(<<"FRA">>)),
+    ?assertEqual(<<"ENG">>, iso_erlang:to_language_alpha_3("ENG")),
+    ?assertEqual(<<"FRA">>, iso_erlang:to_language_alpha_3("FRA")).
+
+to_alpha_3_incorrect_test() ->
+    ?assertError(_, iso_erlang:to_language_alpha_3(<<"EGR">>)),
+    ?assertError(_, iso_erlang:to_language_alpha_3(<<"AAA">>)),
+    ?assertError(_, iso_erlang:to_language_alpha_3("EGs")),
+    ?assertError(_, iso_erlang:to_language_alpha_3("AAA")).
+
+alpha_2_to_name_correct_test() ->
+    ?assertEqual(<<"English">>, iso_erlang:language_alpha_2_to_name(<<"EN">>)),
+    ?assertEqual(<<"Arabic">>, iso_erlang:language_alpha_2_to_name(<<"AR">>)),
+    ?assertEqual(<<"English">>, iso_erlang:language_alpha_2_to_name("EN")),
+    ?assertEqual(<<"Arabic">>, iso_erlang:language_alpha_2_to_name("AR")).
+
+alpha_2_to_name_incorrect_test() ->
+    ?assertError(_, iso_erlang:language_alpha_2_to_name(<<"EG">>)),
+    ?assertError(_, iso_erlang:language_alpha_2_to_name(<<"USA">>)),
+    ?assertError(_, iso_erlang:language_alpha_2_to_name("ABC")),
+    ?assertError(_, iso_erlang:language_alpha_2_to_name("AAA")).
+
+alpha_3_to_name_correct_test() ->
+    ?assertEqual(<<"Egyptian (Ancient)">>, iso_erlang:language_alpha_3_to_name(<<"EGY">>)),
+    ?assertEqual(<<"English">>, iso_erlang:language_alpha_3_to_name(<<"ENG">>)),
+    ?assertEqual(<<"Egyptian (Ancient)">>, iso_erlang:language_alpha_3_to_name("EGY")),
+    ?assertEqual(<<"English">>, iso_erlang:language_alpha_3_to_name("ENG")).
+
+alpha_3_to_name_incorrect_test() ->
+    ?assertError(_, iso_erlang:language_alpha_3_to_name(<<"EG">>)),
+    ?assertError(_, iso_erlang:language_alpha_3_to_name(<<"US">>)),
+    ?assertError(_, iso_erlang:language_alpha_3_to_name("ABA")),
+    ?assertError(_, iso_erlang:language_alpha_3_to_name("AA")).
+
+to_language_name_correct_test() ->
+    ?assertEqual(<<"Egyptian (Ancient)">>, iso_erlang:to_language_name(<<"EGY">>)),
+    ?assertEqual(<<"English">>, iso_erlang:to_language_name(<<"ENG">>)),
+    ?assertEqual(<<"Egyptian (Ancient)">>, iso_erlang:to_language_name("EGY")),
+    ?assertEqual(<<"English">>, iso_erlang:to_language_name("ENG")),
+    ?assertEqual(<<"English">>, iso_erlang:to_language_name(<<"EN">>)),
+    ?assertEqual(<<"Arabic">>, iso_erlang:to_language_name(<<"AR">>)),
+    ?assertEqual(<<"English">>, iso_erlang:to_language_name("EN")),
+    ?assertEqual(<<"Arabic">>, iso_erlang:to_language_name("AR")).
+
+to_language_name_incorrect_test() ->
+    ?assertError(_, iso_erlang:to_language_name(<<"ABC">>)),
+    ?assertError(_, iso_erlang:to_language_name(<<"AAA">>)),
+    ?assertError(_, iso_erlang:to_language_name("ABC")),
+    ?assertError(_, iso_erlang:to_language_name("AAA")).
+
+normalize_test() ->
+    ?assertEqual(<<"HELLO">>, iso_erlang:normalize(<<"Hello">>)),
+    ?assertEqual(<<"HELLO">>, iso_erlang:normalize(<<"HeLlO">>)),
+    ?assertEqual(<<"HELLO">>, iso_erlang:normalize("hello")),
+    ?assertEqual(<<"HELLO">>, iso_erlang:normalize("HeLlO")).
+
 get_language_lists_test() ->
     ?assertEqual(184, length(iso_erlang:get_alpha_2_language_list())),
     ?assertEqual(488, length(iso_erlang:get_alpha_3_language_list())),
